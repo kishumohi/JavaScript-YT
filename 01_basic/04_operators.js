ff5fb18337e47a1ba727ce70cb892ac1111275ac
kishan <kishankalavadiya@gmail.com>
2023-11-05 22:03:48 +0530

    operator

---
+++ 01_basic/04_operators.js
@@ -0,0 +1,38 @@
+// Arithmetic Operators   + / * -
+// Assignment Operators 
+
+// console.log(x += y);
+// console.log(x *= y);
+// console.log(x /= y);
+// console.log(x -= y);
+
+// Comparison Operators
+// console.log(x == y);
+// console.log(x != y);
+// console.log(x === y);
+
+// String Operators
+// let text1 = "Nice Day";
+// let text3 = "What a very " + text1;
+
+// console.log(text3);
+
+
+// Logical Operators
+let x = 5;
+let y = 10;
+// console.log((x < 1) && (y > 5));
+// console.log((x < 1) || (y > 5));
+
+
+// Bitwise Operators
+
+
+// Ternary Operators
+
+
+let ternyope = (5 != 6) ? '$2.00' : '$10.00';
+//               true       result    result
+console.log(ternyope);
+
+
